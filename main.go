bc3bddd0fd48cc914ed7fc5b5feba9b2c6c8af37
daniilak <daniilakk@gmail.com>
2018-12-25 23:50:51 +0300

    Фикс ридми, фикс конфиг файла (более не нужен номер доски ), добавлена общая функция для работы с апи трелло

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -47,31 +48,46 @@ func getConfigList() (config Config) {
 	return config
 }
 
+// config getting
 var config = getConfigList()
 
+// IDMember getting
+var IDMember = getMemberID()
+
+func parseInt(s string) (i int) {
+	strconv.Atoi(s)
+	strconv.ParseInt(s, 10, 64)
+	fmt.Sscan(s, &i)
+
+	return i
+}
+
 func main() {
-	id := getMemberID()
-	args := os.Args[1:]
-	if len(args) > 0 {
-		switch par := args[0]; par {
+
+	if len(os.Args) > 1 {
+		switch param := os.Args[1]; param {
+
 		case "-b":
-			getListBoards(id)
+			boards := getListBoards(IDMember)
+			for i := 0; i < len(boards); i++ {
+				fmt.Printf("%d %+v\n", i+1, boards[i].Name)
+			}
 			break
+
 		case "-c":
-			if len(args[1]) > 0 {
-				getListCards(args[1])
-			} else {
-				getListCards("1")
+			if len(os.Args) < 2 {
+				fmt.Println("needed number")
+				return
+			}
+			s := os.Args[2]
+			cardID := parseInt(s)
+			cards := getListCards(IDMember, cardID)
+			for i := 0; i < len(cards); i++ {
+				fmt.Printf("%d %+v\n", i+1, cards[i].Name)
 			}
 			break
 		}
 	}
-}
-
-func getIDCard() {
-	fmt.Print("Enter a number card:")
-	var input float64
-	fmt.Scanf("%f", &input)
 }
 
 func readBody(srcURL string) ([]byte, error) {
@@ -100,54 +116,50 @@ func check(e error) {
 	}
 }
 
-func getListCards(id string) {
-	var request = strings.Join(
-		[]string{
-			"https://api.trello.com/1/boards/",
-			string(config.BoardID),
-			"/cards/?limit=10&fields=name&key=",
-			string(config.AppKey),
-			"&token=",
-			string(config.ClientKey)},
-		"")
-	respBytes, err := readBody(request)
-	var cards []Card
-	err = json.Unmarshal(respBytes, &cards)
+func getListCards(IDMember string, id int) (cards []Card) {
+	boards := getListBoards(IDMember)
+	fmt.Println("SELECT BOARD: ", string(boards[id-1].Name))
+
+	//"/cards/?limit=10&fields=name&key="
+	respBytes := requestTrelloAPI("boards/", string(boards[id-1].ID), "/cards")
+	err := json.Unmarshal(respBytes, &cards)
+
 	if err != nil {
 		fmt.Printf("with creating struct from bytes : %s", err)
 	}
-	for i := 0; i < len(cards); i++ {
-		fmt.Printf("%d %+v\n", i+1, cards[i].Name)
-	}
+
+	return cards
 }
 
-func getListBoards(IDMember string) {
-	var request = strings.Join(
-		[]string{
-			"https://api.trello.com/1/members/",
-			IDMember,
-			"/boards?filter=all&fields=all&lists=none&memberships=none&organization=false&organization_fields=name%2CdisplayName&key=",
-			string(config.AppKey),
-			"&token=",
-			string(config.ClientKey)},
-		"")
-	respBytes, err := readBody(request)
-
+func getListBoards(IDMember string) (boards []Board) {
+	respBytes := requestTrelloAPI("members/", string(IDMember), "/boards")
+	err := json.Unmarshal(respBytes, &boards)
 	if err != nil {
-		fmt.Printf("err: %s", err)
+		fmt.Printf("with creating struct from bytes : %s", err)
 	}
-	var boards []Board
-	err = json.Unmarshal(respBytes, &boards)
-	for i := 0; i < len(boards); i++ {
-		fmt.Printf("%d %+v\n", i+1, boards[i].Name)
-	}
+	return boards
 }
 
 func getMemberID() (id string) {
+	respBytes := requestTrelloAPI("tokens/", string(config.ClientKey))
+
+	var member Member
+	err := json.Unmarshal(respBytes, &member)
+	if err != nil {
+		fmt.Printf("with creating struct from bytes : %s", err)
+	}
+	return member.IDMember
+}
+
+func requestTrelloAPI(args ...string) (respBytes []byte) {
+	var str strings.Builder
+	for i := 0; i < len(args); i++ {
+		str.WriteString(args[i])
+	}
 	var request = strings.Join(
 		[]string{
-			"https://api.trello.com/1/tokens/",
-			string(config.ClientKey),
+			"https://api.trello.com/1/",
+			str.String(),
 			"?token=",
 			string(config.ClientKey),
 			"&key=",
@@ -157,10 +169,5 @@ func getMemberID() (id string) {
 	if err != nil {
 		fmt.Printf("err: %s", err)
 	}
-	var member Member
-	err = json.Unmarshal(respBytes, &member)
-	if err != nil {
-		fmt.Printf("with creating struct from bytes : %s", err)
-	}
-	return member.IDMember
+	return respBytes
 }
