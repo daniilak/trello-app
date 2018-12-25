1c19fc0f3c78da741786f8d28f9e41dc13585eb7
daniilak <daniilakk@gmail.com>
2018-12-25 22:29:06 +0300

    Добавлены \n1) чтение команд с командной строки \n2) сделаны функции загрузки json в структуру \n3) сделаны методы получения айди пользователя, списка досок и списка листов

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -22,8 +23,49 @@ type Card struct {
 	Name string
 }
 
+// Member structure
+type Member struct {
+	ID       string
+	IDMember string
+}
+
+// Board structure
+type Board struct {
+	ID             string
+	URL            string
+	Name           string
+	Desc           string
+	DescData       string
+	Closed         bool
+	IDOrganization string
+}
+
+func getConfigList() (config Config) {
+	dat, err := ioutil.ReadFile("config.json")
+	check(err)
+	err = json.Unmarshal(dat, &config)
+	return config
+}
+
+var config = getConfigList()
+
 func main() {
-	getListCards()
+	id := getMemberID()
+	args := os.Args[1:]
+	if len(args) > 0 {
+		switch par := args[0]; par {
+		case "-b":
+			getListBoards(id)
+			break
+		case "-c":
+			if len(args[1]) > 0 {
+				getListCards(args[1])
+			} else {
+				getListCards("1")
+			}
+			break
+		}
+	}
 }
 
 func getIDCard() {
@@ -43,7 +85,6 @@ func readBody(srcURL string) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("with read body: %s: %v", resp.Body, err)
 	}
-
 	return body, nil
 }
 
@@ -59,13 +100,7 @@ func check(e error) {
 	}
 }
 
-func getListCards() {
-
-	dat, err := ioutil.ReadFile("config.json")
-	check(err)
-	var config Config
-	err = json.Unmarshal(dat, &config)
-
+func getListCards(id string) {
 	var request = strings.Join(
 		[]string{
 			"https://api.trello.com/1/boards/",
@@ -76,7 +111,6 @@ func getListCards() {
 			string(config.ClientKey)},
 		"")
 	respBytes, err := readBody(request)
-
 	var cards []Card
 	err = json.Unmarshal(respBytes, &cards)
 	if err != nil {
@@ -85,5 +119,48 @@ func getListCards() {
 	for i := 0; i < len(cards); i++ {
 		fmt.Printf("%d %+v\n", i+1, cards[i].Name)
 	}
+}
 
+func getListBoards(IDMember string) {
+	var request = strings.Join(
+		[]string{
+			"https://api.trello.com/1/members/",
+			IDMember,
+			"/boards?filter=all&fields=all&lists=none&memberships=none&organization=false&organization_fields=name%2CdisplayName&key=",
+			string(config.AppKey),
+			"&token=",
+			string(config.ClientKey)},
+		"")
+	respBytes, err := readBody(request)
+
+	if err != nil {
+		fmt.Printf("err: %s", err)
+	}
+	var boards []Board
+	err = json.Unmarshal(respBytes, &boards)
+	for i := 0; i < len(boards); i++ {
+		fmt.Printf("%d %+v\n", i+1, boards[i].Name)
+	}
+}
+
+func getMemberID() (id string) {
+	var request = strings.Join(
+		[]string{
+			"https://api.trello.com/1/tokens/",
+			string(config.ClientKey),
+			"?token=",
+			string(config.ClientKey),
+			"&key=",
+			string(config.AppKey)},
+		"")
+	respBytes, err := readBody(request)
+	if err != nil {
+		fmt.Printf("err: %s", err)
+	}
+	var member Member
+	err = json.Unmarshal(respBytes, &member)
+	if err != nil {
+		fmt.Printf("with creating struct from bytes : %s", err)
+	}
+	return member.IDMember
 }
